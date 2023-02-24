4f02308bf85a0277f7b307a5dbd8828a4db16bb3
junyoung <jy940608@naver.com>
2023-02-25 00:26:24 +0900

    add: 관리자페이지 추가

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "kind",
+    )
+    list_filter = ("kind",)
--- experiences/admin.py
+++ experiences/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Experience, Perk
 
-# Register your models here.
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "price",
+        "start",
+        "end",
+        "created_at",
+    )
+
+    list_filter = ("category",)
+
+
+@admin.register(Perk)
+class PerkAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "name",
+        "details",
+        "explanation",
+    )
