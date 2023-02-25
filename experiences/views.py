8de9ed9098f371e38637084464fc1ec250c26914
junyoung <jy940608@naver.com>
2023-02-25 23:47:06 +0900

    add: experience API

--- experiences/views.py
+++ experiences/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Perk
+from .serializers import PerkSerializer
+
+
+class Perks(APIView):
+    def get(self, request):
+        all_perks = Perk.objects.all()
+        serializer = PerkSerializer(all_perks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PerkSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class PerkDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Perk, pk=pk)
+
+    def get(self, request, pk):
+        perk = self.get_object(pk)
+        serializer = PerkSerializer(perk)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        perk = self.get_object(pk)
+        serializer = PerkSerializer(perk, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        perk = self.get_object(pk)
+        perk.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
