b7cdca0cf24491985d5a9944b34020a11435a8a7
junyoung <jy940608@naver.com>
2023-02-26 04:44:05 +0900

    add: Room CRUD API

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,93 @@
+from django.db import transaction
+from rest_framework import serializers
+from rest_framework.exceptions import ParseError
+from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+
+from categories.models import Category
+from categories.serializers import CategorySerializer
+from users.serializers import TinyUserSerializer
+from .models import Amenity, Room
 
 
 class AmenitySerializer(ModelSerializer):
     class Meta:
         model = Amenity
         fields = "__all__"
+
+
+class RoomDetailSerializer(serializers.ModelSerializer):
+    owner = TinyUserSerializer(read_only=True)
+    amenities = AmenitySerializer(
+        read_only=True,
+        many=True,
+    )
+
+    # category 필드를 PrimaryKeyRelatedField로 정의하여 CategorySerializer를 사용합니다.
+    category = PrimaryKeyRelatedField(queryset=Category.objects.all())
+
+    class Meta:
+        model = Room
+        fields = "__all__"
+
+    def validate(self, data):
+        category = data.get("category")
+        if category and category.kind != Category.CategoryKindChoices.ROOMS:
+            raise serializers.ValidationError("The category kind should be 'rooms'")
+        return data
+
+    def create(self, validated_data):
+        amenities = self.context['request'].data.get("amenities", None)
+        room = Room.objects.create(**validated_data)
+
+        if amenities:
+            amenity_objs = Amenity.objects.filter(id__in=amenities)
+            room.amenities.set(amenity_objs)
+
+        # try:
+        #     with transaction.atomic():
+        #         room = Room.objects.create(**validated_data)
+        #         for amenity_pk in amenities:
+        #             amenity = Amenity.objects.get(pk=amenity_pk)
+        #             room.amenities.add(amenity)
+        #
+        # except Exception as e:
+        #     raise ParseError("Amenity not found")
+
+        return room
+
+    def update(self, instance, validated_data):
+        category = validated_data.pop("category", None)
+        if category:
+            instance.category = category
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        amenities = self.context['request'].data.get("amenities", None)
+
+        if amenities:
+            amenity_objs = Amenity.objects.filter(id__in=amenities)
+            instance.amenities.set(amenity_objs)
+
+        return instance
+
+    def to_representation(self, instance):
+        # RoomCreateSerializer의 to_representation 메서드를 오버라이드하여 category 필드를 CategorySerializer로 변경합니다.
+        data = super().to_representation(instance)
+        data['category'] = CategorySerializer(instance.category).data
+        return data
+
+
+class RoomListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+        )
--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,11 @@
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
-from rooms.models import Amenity
+
+from rooms import serializers
+from rooms.models import Amenity, Room
 from rooms.serializers import AmenitySerializer
 
 
@@ -42,4 +45,56 @@ class AmenityDetail(APIView):
     def delete(self, request, pk):
         amenity = self.get_object(pk)
         amenity.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class Rooms(APIView):
+    def get(self, request):
+        all_rooms = Room.objects.all()
+        serializer = serializers.RoomListSerializer(
+            all_rooms,
+            many=True,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = serializers.RoomDetailSerializer(data=request.data, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save(owner=self.request.user)
+        return Response(serializer.data)
+
+
+class RoomDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Room, pk=pk)
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        serializer = serializers.RoomDetailSerializer(
+            room,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        room = self.get_object(pk)
+        if room.owner != request.user:
+            raise PermissionDenied
+
+        serializer = serializers.RoomDetailSerializer(
+            room,
+            data=request.data,
+            context={"request": request},
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save(owner=self.request.user)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        room = self.get_object(pk)
+        if room.owner != request.user:
+            raise PermissionDenied
+        room.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
